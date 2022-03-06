3f29c07051d127e0bc4ee44949e26fc4bca19c83
Asım Kaymak <kaymakasm@gmail.com>
2022-03-06 03:32:45 +0300

    todo added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wordle_game/features/home/model/word_model.dart';
 import 'features/home/home.dart';
+//TODO WORD GUESS LETTER COLOR BUG
 
 void main() => runApp(
       MultiProvider(
